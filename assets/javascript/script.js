96136685c372c0be854d803b6e48b08d79d0c3b6
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2025-02-25 16:36:40 +0500

    Navbar completed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdown = document.querySelector(".dropdown");
-    const button = document.querySelector(".dropdown-btn");
-
-    button.addEventListener("click", function (event) {
-        event.stopPropagation();
-        dropdown.classList.toggle("active");
-    });
-
-    document.addEventListener("click", function () {
-        dropdown.classList.remove("active");
-    });
+let showAllBtn = document.getElementsByClassName(`select`)[0];
+let showAllText = document.getElementsByClassName("selected")[0];
+let dropDownOption = document.getElementsByClassName(`options`)[0];
+let options = document.querySelectorAll('.option');
+let current = showAllText.innerHTML;
+options.forEach((option) => {
+    option.addEventListener('click', (e) => {
+        showAllText.innerHTML = option.innerHTML;
+        dropDownOption.classList.remove('show');
+    })
+})
+showAllBtn.addEventListener("click", (e) => {
+    dropDownOption.classList.toggle("show");
 });
